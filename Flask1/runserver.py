a163f411b8e05358f36e7f933b01be77d436d0a0
huimengli <2913394404@qq.com>
2020-09-04 16:22:46 +0800

    flask内网穿透没法用js调用其他js文件 所以只能用上个版本的lt_code了

--- Flask1/runserver.py
+++ Flask1/runserver.py
@@ -6,9 +6,9 @@ from os import environ
 from Flask1 import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
+    HOST = environ.get('0.0.0.0', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '8000'))
     except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT,debug=True);
+        PORT = 8000
+    app.run(HOST, PORT,debug=False);
