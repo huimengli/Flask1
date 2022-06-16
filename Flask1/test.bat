ea88deac0e5b77ac2a8f94d8af5f898221f7d5be
听 楼 <2913394404@qq.com>
2022-06-16 15:50:12 +0800

    修改了加速度计的掉能够用

---
+++ Flask1/test.bat
@@ -0,0 +1,16 @@
+:这里需要启动一个新的窗口启动内容natapp服务器:
+:start cmd /k echo Hello, World! 窗口结束后不会关闭:
+:start cmd /C pause 窗口结束后自动关闭:
+:启动Natapp隧道:
+:start cmd /C %~dp0natapp -authtoken=9987c65e46bb5b5f:
+:启动网页查看网站:
+start cmd /C explorer http://localhost:8000
+:loop
+:启动网页服务器:
+%~dp0env\Scripts\python.exe %~dp0runserver.py
+goto loop
+:为了查看错误信息:
+pause
+:explorer http://slow.keliit.top:
+:网站服务器结束后关闭所有cmd窗口:
+:taskkill /f /im cmd.exe:
